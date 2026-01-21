64415d7d4debc84e6f2e08cbc143af2149354152
Marin915 <marin9.campos@gmail.com>
2026-01-20 23:32:57 -0600

    conexion bd

---
+++ src/main/java/mx/insabit/ValidacionMateriales/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+
+package mx.insabit.ValidacionMateriales.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+/**
+ * Configuración de seguridad sin autenticación.
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(csrf -> csrf.disable())
+            .cors(Customizer.withDefaults())
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            );
+
+        return http.build();
+    }
+}
