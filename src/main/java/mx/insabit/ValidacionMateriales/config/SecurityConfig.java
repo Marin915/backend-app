5eb56e36a40656c797dd3c48c1a261760310cdfc
Marin915 <marin9.campos@gmail.com>
2026-01-21 23:12:39 -0600

    login terminado 3

--- src/main/java/mx/insabit/ValidacionMateriales/config/SecurityConfig.java
+++ src/main/java/mx/insabit/ValidacionMateriales/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package mx.insabit.ValidacionMateriales.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -21,15 +23,18 @@ public class SecurityConfig {
 
 @Bean
 public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
-      .csrf(csrf -> csrf.disable())
-      .authorizeHttpRequests(auth -> auth
-          .requestMatchers("/api/public/**").permitAll()
-          .anyRequest().authenticated()
-      )
-      .httpBasic(withDefaults());  // aquí usamos el método helper
-    return http.build();
-}
+  
+        http
+            .cors(Customizer.withDefaults()) // ✅ FORMA CORRECTA
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .requestMatchers("/api/**").permitAll()
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
 
 
 
--- src/main/java/mx/insabit/ValidacionMateriales/config/WebConfig.java
+++ src/main/java/mx/insabit/ValidacionMateriales/config/WebConfig.java
@@ -6,20 +6,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://inventario-frontend-pz47.onrender.com")
-                      //.allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://inventario-frontend-pz47.onrender.com")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*");
     }
 }
